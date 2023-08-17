cd06098c571a07a048298a73012c912566b0411c
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2023-08-17 18:40:34 +0900

    changed from deb to rel

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-docker compose build --progress plain
+docker compose --progress plain build
 docker compose up -d
 docker compose exec dotnet-dev /source/dotnet60/build.sh
+
+#docker compose exec dotnet-run bash -c "cat ./isql.txt | isql irisdatasource"
--- dotnet60/build.sh
+++ dotnet60/build.sh
@@ -4,9 +4,9 @@ cp -fR /source/ODBC/* ./ODBC
 cp /source/dotnet60/ADO.csproj ./ADO/ADO.csproj
 cp /source/dotnet60/ODBC.csproj ./ODBC/ODBC.csproj
 dotnet restore ADO/ADO.csproj
-dotnet publish ADO/ADO.csproj -c debug -o /app
+dotnet publish ADO/ADO.csproj -c release -o /app
 dotnet restore ODBC/ODBC.csproj
-dotnet publish ODBC/ODBC.csproj -c debug -o /app
+dotnet publish ODBC/ODBC.csproj -c release -o /app
 
 echo "dotnet /app/ADO.dll to run"
 
