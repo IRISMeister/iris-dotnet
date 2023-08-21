c89b096f9a8f6f8cfb662a7a7d56732768594eea
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2023-08-21 11:54:59 +0900

    automatically create tables

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ docker compose --progress plain build
 docker compose up -d
 docker compose exec dotnet-dev /source/dotnet60/build.sh
 
-#docker compose exec dotnet-run bash -c "cat ./isql.txt | isql irisdatasource"
+docker compose exec dotnet-run bash -c "cat ./isql.txt | isql irisdatasource"
